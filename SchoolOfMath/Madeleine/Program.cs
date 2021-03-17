0152a35d9dd5037759639b9f90e72322189c7b75
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-17 23:02:33 +0300

    Madeleine isimli 3ncü Worker servis eklendi

---
+++ SchoolOfMath/Madeleine/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SchoolOfRock;
+using System;
+
+namespace Madeleine
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddGrpcClient<PalindromeFinder.PalindromeFinderClient>(options =>
+                    {
+                        options.Address = hostContext.Configuration.GetServiceUri("einstein") ?? new Uri("https://localhost:7001");
+                    });
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
