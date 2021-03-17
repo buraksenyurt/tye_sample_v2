e911da5c4e004c79d5713cc58595a91a7974793e
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-17 23:47:03 +0300

    gRPC servisine Redis desteği eklendi

--- SchoolOfMath/Einstein/Services/PalindromeFinderService.cs
+++ SchoolOfMath/Einstein/Services/PalindromeFinderService.cs
@@ -1,6 +1,8 @@
-using Grpc.Core;
+﻿using Grpc.Core;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
 using SchoolOfRock;
+using System;
 using System.Threading.Tasks;
 
 namespace Einstein
@@ -9,24 +11,45 @@ namespace Einstein
         : PalindromeFinder.PalindromeFinderBase
     {
         private readonly ILogger<PalindromeFinderService> _logger;
-        public PalindromeFinderService(ILogger<PalindromeFinderService> logger)
+        private readonly IDistributedCache _cache;
+        private readonly PalindromeReply True = new() { IsPalindrome = true };
+        private readonly PalindromeReply False = new() { IsPalindrome = false };
+        public PalindromeFinderService(ILogger<PalindromeFinderService> logger, IDistributedCache cache)
         {
             _logger = logger;
+            _cache = cache; //Dağıtık cache servisi olarak Redis konumlanacak. Startup'ta onu ekledik çünkü.
         }
 
         public override async Task<PalindromeReply> IsItPalindrome(PalindromeRequest request, ServerCallContext context)
         {
             long r, sum = 0, t;
-            var num = request.Number;
-            for (t = num; num != 0; num /= 10)
+            var number = request.Number;
+
+            var inCache = await _cache.GetStringAsync(request.Number.ToString()); // bu sayı Redis Cache'te var mı?
+            if (inCache=="YES")
             {
-                r = num % 10;
+                _logger.LogInformation($"{request.Number} palindrom bir sayıdır ve şu an Redis'ten getiriyorum. Hesap etmeye gerek yok");
+                return True;
+            }
+
+            for (t = number; number != 0; number /= 10)
+            {
+                r = number % 10;
                 sum = sum * 10 + r;
             }
             if (t == sum)
-                return new PalindromeReply { IsPalindrome = true };
+            {
+                _logger.LogInformation($"{request.Number} palindrom bir sayı ama Redis cache'e atılmamış. Şimdi ekleyeceğim.");
+                // Sayı adını Key olarak kullanıp Cache'e atıyoruz ve ona value olarak YES değerini atıyoruz.
+                await _cache.SetStringAsync(request.Number.ToString(), "YES", new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(60)
+                });
+                // Palindrome sayı ise onu Redis Cache'e atıyoruz.
+                return True;
+            }
             else
-                return new PalindromeReply { IsPalindrome = false };
+                return False;
         }
     }
 }
--- SchoolOfMath/Einstein/Startup.cs
+++ SchoolOfMath/Einstein/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,14 +13,22 @@ namespace Einstein
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+
+            // Redis bildirimini yaptık. PalindromeFinderService, consturctor'dan alacak.
+            services.AddStackExchangeRedisCache(o =>
+            {
+                o.Configuration = Configuration.GetConnectionString("redis") ?? "localhost:6379";
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
