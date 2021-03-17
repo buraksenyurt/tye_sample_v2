cdfa6dfd4712d8c76184513e1a4c182921ee5770
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-17 22:37:04 +0300

    Worker kodları düzenlendi.

---
+++ SchoolOfMath/Evelyne/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SchoolOfRock;
+using System;
+
+namespace Evelyne
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    // gRPC istemcisini çalışma zamanına ekliyoruz
+                    services.AddGrpcClient<PalindromeFinder.PalindromeFinderClient>(options =>
+                    {
+                        // servis adresini Tye extension fonksiyonu üstünden çekiyoruz
+                        // Eğer debug modda çalışıyorsak (tye.yaml olmadan tye run ile mesela) einstein'ın 7001 nolu adresine yönlendiriyoruz.
+                        options.Address = hostContext.Configuration.GetServiceUri("einstein") ?? new Uri("https://localhost:7001");
+                    });
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ SchoolOfMath/Evelyne/Worker.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SchoolOfRock;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Evelyne
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly PalindromeFinder.PalindromeFinderClient _client;
+
+        // gRPC servisini constructor üzerinden içeriye enjekte ediyoruz
+        public Worker(ILogger<Worker> logger,PalindromeFinder.PalindromeFinderClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // Servisin ayağa kalkması için bir süre bekletiyoruz. Makine soğuk. 
+            await Task.Delay(TimeSpan.FromSeconds(30), stoppingToken);
+            _logger.LogInformation("### Servis başlatılıyor ###");
+            long number = 1; // Evelyne, 1den itibaren sayıları hesap etmeye başlayacak
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var response = await _client.IsItPalindromeAsync(new PalindromeRequest { Number = number });
+                    _logger.LogInformation($"{number}, palindrom bir sayıdır önermesinin cevabı = {response.IsPalindrome}\r");
+                }
+                catch (Exception ex)
+                {
+                    // Bir exception oluşması halinde Worker'ın işleyişini durduracağız
+                    if (stoppingToken.IsCancellationRequested) 
+                        return;
+                    
+                    _logger.LogError(-1, ex, "Bir hata oluştu. Worker çalışması sonlanıyor.");
+                    await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+                }
+
+                number++;
+
+                if (stoppingToken.IsCancellationRequested) 
+                    break;
+
+                await Task.Delay(TimeSpan.FromMilliseconds(100), stoppingToken); // İstemci 100 milisaniyede bir ateş edecek :P
+            }
+        }
+    }
+}
