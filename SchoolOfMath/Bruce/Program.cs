5d063fda38ef6d21391cc3ca98d71fefbfa7e992
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-17 22:47:05 +0300

    Bruce isimli diğer Worker servisi eklendi.

---
+++ SchoolOfMath/Bruce/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SchoolOfRock;
+using System;
+
+namespace Bruce
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddGrpcClient<PalindromeFinder.PalindromeFinderClient>(options =>
+                    {
+                        options.Address = hostContext.Configuration.GetServiceUri("einstein") ?? new Uri("https://localhost:7001");
+                    });
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
